88dd03b9437a9ff7a81e1d905df5c3cf439e7e40
a-kashif-ahmed <kashifmsd7@gmail.com>
2026-05-05 20:55:59 +0530

    Trying / rio

--- utils/connect_db.py
+++ utils/connect_db.py
@@ -5,11 +5,9 @@ import os
 
 from pathlib import Path
 import sqlite3
-
-# On Vercel, only /tmp is writable
-DB_PATH = get_parent_path()
-
-def get_connection():
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
+DB_PATH = get_parent_path() / "database" / "app.db" 
+os.makedirs(DB_PATH.parent, exist_ok=True) 
+def get_connection(): 
+    conn = sqlite3.connect(DB_PATH) 
+    conn.row_factory = sqlite3.Row 
     return conn
