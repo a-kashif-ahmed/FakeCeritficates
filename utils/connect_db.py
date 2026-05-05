52f31c09aa4e49bf418cda4f6a3d1973c29cee11
a-kashif-ahmed <kashifmsd7@gmail.com>
2026-05-05 18:31:39 +0530

    Make tmp db at ver

--- utils/connect_db.py
+++ utils/connect_db.py
@@ -9,9 +9,6 @@ import sqlite3
 # On Vercel, only /tmp is writable
 DB_PATH = Path("/tmp/app.db")
 
-
-
-
 def get_connection():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
